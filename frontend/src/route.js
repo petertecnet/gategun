e3a74e50f9d039b1b0a3cf740f19dfdbefefdcdf
petertecnet <pivow@hotmail.com>
2023-05-14 17:25:07 -0300

    Commit of login job to chat GPT knoledge

--- frontend/src/route.js
+++ frontend/src/route.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LoginView from './views/LoginView.vue';
+import LoginView from '@/views/LoginView.vue';
+
 
 const routes = [
   {
